580bcad96a54ba999ba9c63a52bb8b742742467f
Alejandro Tejada <tej17584@uvg.edu.gt>
2020-07-23 16:59:15 -0600

    Set de Directorio, set de librerias y de limpieza

---
+++ Laboratorio1/Laboratorio1-2020.R
@@ -0,0 +1,40 @@
+###################################################
+## Guatemala, julio del 2020
+## Data Science 
+## Alejandro Tejada 17584
+## Diego Sevilla 17238
+## Rodrigo Samayoa
+## Laboratorio No.1
+## Catedratico: Lynette Garc?a
+###################################################
+
+
+##-----------------ZONA LIBRERIAS---------------------------
+library(dplyr)
+library(rpart)
+library(tree)
+library(rpart.plot)
+library(dummies)
+library(nortest)
+library(caret)
+library(corrplot)
+library(e1071)
+library(nnet)
+library(RWeka)
+library(neural)
+library(dummy)
+library(neuralnet)
+library(stringi)
+library(stringr)
+##------------------FIN ZONA LIBRERIAS------------------
+
+
+##-----------------ZONA DIRECTORIOS---------------------------
+getwd()
+setwd("C:/Users/josea/Desktop/Universidad/2020/DataScience/LabsDataScience/Laboratorio1")
+#setwd("C:/Users/josea/Desktop/Universidad/2020/DataScience/Proyecto1/DataScienceProject1/CSV")
+test <-read.csv("test.csv",stringsAsFactors = FALSE, na.strings = TRUE)
+train <-read.csv("train.csv",stringsAsFactors = FALSE, na.strings = TRUE)
+head(test[,1:3])
+head(train[,1:3])
+
