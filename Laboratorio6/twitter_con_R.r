ea6df3b4eb84f7f31abfec575ad86ee69f752977
toti <sam17332@uvg.edu.gt>
2020-09-03 17:18:47 -0600

    Comienzo de limpieza.

--- Laboratorio6/twitter_con_R.r
+++ Laboratorio6/twitter_con_R.r
@@ -15,10 +15,21 @@ accessToken <-	"409713602-aBMMJOsk0Vc1zWCOUSyBPz56nnboWjHLYsyKNeM9"
 accessTokenSecret <-	"XK3jfmZjB8QOGAoTMndpZLs3HFvjIhtmi02gAYCf44sUH"
 
 setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
-tweets<-searchTwitteR("@totisama",n=150,lang = "es")
+tweets<-searchTwitteR("traficogt",n=150,lang = "es")
 tweetdf<-twListToDF(tweets)
 dif_UTC_gt<-6*60*60
 tweetdf$created2<-tweetdf$created-dif_UTC_gt
 write.csv(tweetdf,"tweets.csv", fileEncoding = "UTF-8")
 
+#Leer csv
 data <- read.csv("tweets.csv", fileEncoding = "UTF-8")
+
+##limpieza de datos
+#Quitar columnas
+data<-data[,-c(5, 8, 9, 10, 11, 12, 16, 17)]
+
+#Quitar Emojis
+data$text <- iconv(data$text, "latin1", "ASCII", sub="")
+
+
+View(data)
