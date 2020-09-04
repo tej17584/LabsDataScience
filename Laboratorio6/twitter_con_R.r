286a337c55a20594081f3c3224bc1e864a6dc68c
toti <sam17332@uvg.edu.gt>
2020-09-03 18:14:11 -0600

    Limpieza de data.

--- Laboratorio6/twitter_con_R.r
+++ Laboratorio6/twitter_con_R.r
@@ -15,21 +15,41 @@ accessToken <-	"409713602-aBMMJOsk0Vc1zWCOUSyBPz56nnboWjHLYsyKNeM9"
 accessTokenSecret <-	"XK3jfmZjB8QOGAoTMndpZLs3HFvjIhtmi02gAYCf44sUH"
 
 setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
-tweets<-searchTwitteR("traficogt",n=150,lang = "es")
+tweets<-searchTwitteR("traficogt", n=150, lang = "es")
 tweetdf<-twListToDF(tweets)
 dif_UTC_gt<-6*60*60
 tweetdf$created2<-tweetdf$created-dif_UTC_gt
 write.csv(tweetdf,"tweets.csv", fileEncoding = "UTF-8")
 
-#Leer csv
+## Leer csv
 data <- read.csv("tweets.csv", fileEncoding = "UTF-8")
 
-##limpieza de datos
-#Quitar columnas
+## limpieza de datos
+# Quitar columnas
 data<-data[,-c(5, 8, 9, 10, 11, 12, 16, 17)]
 
-#Quitar Emojis
+# Quitar URLs
+data$text <- gsub("(s?)(f|ht)tp(s?)://\\S+\\b", "", data$text)
+
+# Quitar #
+data$text <- gsub('#\\S+', '', data$text)
+
+# Quitar palabras o caracteres que no sirven
+data$text <- gsub("RT", "", data$text)
+data$text <- gsub("/", "", data$text)
+data$text <- gsub("|", "", data$text)
+
+# Quitar Emojis
 data$text <- iconv(data$text, "latin1", "ASCII", sub="")
 
+# Quitar tildes
+data$text <- gsub("(á)|(Á)", "a", data$text)
+data$text <- gsub("(é)|(É)", "e", data$text)
+data$text <- gsub("(í)|(Í)", "i", data$text)
+data$text <- gsub("(ó)|(Ó)", "o", data$text)
+data$text <- gsub("(ú)|(Ú)", "u", data$text)
+
+# Convertir a mayusculas
+data$text <- toupper(data$text)
 
 View(data)
